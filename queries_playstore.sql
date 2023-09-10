dde9f708a4a720b3883095735dcf9ceb0b80a98d
EricBritt1 <britteric055@gmail.com>
2023-09-10 00:15:59 -0700

    complete sql 22.2 assignment

---
+++ queries_playstore.sql
@@ -0,0 +1,15 @@
+-- Comments in SQL Start with dash-dash --
+-- SELECT app_name, id FROM analytics WHERE id = 1880; --
+-- SELECT app_name, id FROM analytics WHERE last_updated = 'August 1, 2018'; --
+-- SELECT category, COUNT(app_name) AS app_amount FROM analytics GROUP BY category ORDER BY app_amount Desc LIMIT 5; --
+-- SELECT app_name, reviews FROM analytics WHERE reviews > 66577446 ORDER BY reviews Desc; --
+-- SELECT app_name, reviews, rating FROM analytics WHERE rating >= 4.8 ORDER BY reviews Desc;  The app is Chess Free--
+-- SELECT category, AVG(rating) AS average_rating FROM analytics GROUP BY category ORDER BY average_rating Desc; -- 
+-- SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price Desc; name:Naruto & Boruto FR  price:379.99 rating:2.9 -- SELECT app_name, min_installs, rating FROM analytics WHERE min_installs <= 50 AND rating IS NOT null ORDER BY rating Desc; --
+-- SELECT app_name, rating, reviews FROM analytics WHERE rating < 3 AND reviews > 10000; --
+-- SELECT app_name, price, reviews FROM analytics WHERE price > 0.10 AND price < 1.00 ORDER BY reviews Desc LIMIT 10; --
+-- SELECT app_name, last_updated FROM analytics ORDER BY last_updated Asc LIMIT 1; --
+-- SELECT app_name, price FROM analytics ORDER BY price Desc LIMIT 1; --
+-- SELECT SUM(reviews) AS total_reviews FROM analytics; --
+-- SELECT category, COUNT(app_name) FROM analytics GROUP BY category HAVING COUNT(app_name) > 300 ORDER BY COUNT(app_name) Desc; --
+-- SELECT app_name, min_installs, reviews, (min_installs/reviews) AS proportion FROM analytics WHERE min_installs > 100000 ORDER BY proportion Desc LIMIT 1; --
